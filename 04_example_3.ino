1b592a29359a18cef2797f1bfd074d0453bdbf8f
userhbc <133952550+userhbc@users.noreply.github.com>
2023-09-19 17:00:52 +0900

    Create 04_example_3.ino

---
+++ 04_example_3.ino
@@ -0,0 +1,30 @@
+#define PIN_LED 13
+unsigned int count, toggle;
+
+void setup() {
+  pinMode(PIN_LED, OUTPUT);
+  Serial.begin(9600);
+  while(!Serial){
+    ;
+  }
+  Serial.println("Hello World!");
+  count = toggle = 0;
+  digitalWrite(PIN_LED, toggle);
+}
+
+void loop() {
+  Serial.println(++count);
+  toggle = toggle_state(toggle);
+  digitalWrite(PIN_LED, toggle);
+  delay(1000);
+}
+
+int toggle_state(int toggle){
+  if(toggle == 1){
+    toggle = 0;
+  }
+  else{
+    toggle = 1;
+  }
+    return toggle;
+}

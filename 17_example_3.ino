20a8c609fd06b7c2c74b63177f6248595262f2b6
userhbc <133952550+userhbc@users.noreply.github.com>
2023-11-14 20:53:54 +0900

    Update 17_example_3.ino

--- 17_example_3.ino
+++ 17_example_3.ino
@@ -1,28 +1,28 @@
 #include <Servo.h>
-​
+
 // Arduino pin assignment
-​
+
 #define PIN_IR    0         // IR sensor at Pin A0
 #define PIN_LED   9
 #define PIN_SERVO 10
-​
-#define _DUTY_MIN ????  // servo full clock-wise position (0 degree)
-#define _DUTY_NEU ????  // servo neutral position (90 degree)
-#define _DUTY_MAX ????  // servo full counter-clockwise position (180 degree)
-​
+
+#define _DUTY_MIN 500 // servo full clockwise position (0 degree)
+#define _DUTY_NEU 1500 // servo neutral position (90 degree)
+#define _DUTY_MAX 2500 // servo full counterclockwise position (180 degree)
+
 #define _DIST_MIN  100.0   // minimum distance 100mm
 #define _DIST_MAX  250.0   // maximum distance 250mm
-​
-#define EMA_ALPHA  ?.?      // for EMA Filter
-​
-#define LOOP_INTERVAL ??   // Loop Interval (unit: msec)
-​
+
+#define EMA_ALPHA  0.125      // for EMA Filter
+
+#define LOOP_INTERVAL 20   // Loop Interval (unit: msec)
+
 Servo myservo;
 unsigned long last_loop_time;   // unit: msec
-​
+
 float dist_prev = _DIST_MIN;
 float dist_ema = _DIST_MIN;
-​
+
 void setup()
 {
   pinMode(PIN_LED, OUTPUT);
@@ -32,31 +32,46 @@ void setup()
   
   Serial.begin(1000000);    // 1,000,000 bps
 }
-​
+
 void loop()
 {
   unsigned long time_curr = millis();
   int duty;
   float a_value, dist_raw;
-​
+
   // wait until next event time
   if (time_curr < (last_loop_time + LOOP_INTERVAL))
     return;
   last_loop_time += LOOP_INTERVAL;
-​
+
   a_value = analogRead(PIN_IR);
   dist_raw = ((6762.0 / (a_value - 9.0)) - 4.0) * 10.0;
-​
-  if ... // Put range Filter code here (_DIST_MIN ~ _DIST_MAX)
+  //dist_raw = a_value;
+
+  if ((dist_raw == 0.0) || (dist_raw > _DIST_MAX)) {
+    dist_raw = dist_prev;           // Cut higher than maximum
+    digitalWrite(PIN_LED, 1);       // LED OFF
+  } else if (dist_raw < _DIST_MIN) {
+    dist_raw = dist_prev;           // cut lower than minimum
+    digitalWrite(PIN_LED, 1);       // LED OFF
+  } else {    // In desired Range
+    dist_prev = dist_raw;
+    digitalWrite(PIN_LED, 0);       // LED ON      
+  }
+  // Put range Filter code here (_DIST_MIN ~ _DIST_MAX)
   // and turn on LED if the distance is in the range 
-​
-  dist_ema = // Put EMA filter code here
+
+  dist_ema = (EMA_ALPHA * dist_raw) + ((1.0 - EMA_ALPHA) * dist_ema); // Put EMA filter code here
       
-  //duty = map(dist_ema, _DIST_MIN, _DIST_MAX, _DUTY_MIN, _DUTY_MAX);
-  duty = // Put map() equivalent code here
+  duty = _DUTY_MIN + (
+        (_DUTY_MAX - _DUTY_MIN) 
+        / (float)(_DIST_MAX - _DIST_MIN) 
+        * (dist_ema - _DIST_MIN)
+  );
+  // Put map() equivalent code here
   
   myservo.writeMicroseconds(duty);
-​
+
   Serial.print("_DUTY_MIN:");  Serial.print(_DUTY_MIN);
   Serial.print("_DIST_MIN:");  Serial.print(_DIST_MIN);
   Serial.print(",IR:");        Serial.print(a_value);
